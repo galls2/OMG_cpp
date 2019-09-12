6b8f3c03a32d181e62d677827b233370f55f3cd2
galls2 <galls2@cs.technion.ac.il>
2019-09-12 07:50:45 +0300

    Made API fluent Bugfixes in aigparser

--- code/parsers/aig_parser.cpp
+++ code/parsers/aig_parser.cpp
@@ -19,13 +19,9 @@ AigParser::AigParser(const std::string &aig_path) : _aig_path(aig_path)
   std::vector<std::string> file_lines;
   read_aag(file_lines);
 
-  extract_metadata(file_lines[0]);
+  extract_metadata(file_lines[0]).extract_ap_mapping(file_lines).extract_literals(file_lines);
 
-  extract_ap_mapping(file_lines);
-
-   extract_literals(file_lines);
-
-  std::map<size_t, z3::expr> lit_formulas = get_literal_formulas(file_lines);
+  calc_literal_formulas(file_lines);
 
   std::cout << _metadata[AigMetadata::M] << " " << _metadata[AigMetadata::I]<< std::endl;
 
@@ -35,7 +31,7 @@ AigParser::AigParser(const std::string &aig_path) : _aig_path(aig_path)
   }
 }
 
-void AigParser::extract_literals(const std::vector<std::string>& aag_lines)
+AigParser& AigParser::extract_literals(const std::vector<std::string>& aag_lines)
 {
     for (size_t i = 1; i < 1 + _metadata[I]; ++i)
     {
@@ -49,7 +45,7 @@ void AigParser::extract_literals(const std::vector<std::string>& aag_lines)
         std::vector<std::string> parts;
         split(aag_lines[i], ' ', parts);
         _prev_state_literals.push_back(std::stoul(parts[0]));
-        _prev_state_literals.push_back(std::stoul(parts[1]));
+        _next_state_literals.push_back(std::stoul(parts[1]));
     }
 
     for (size_t i = _metadata[I] + _metadata[L] + 1; i < 1 + _metadata[L] + _metadata[O] + _metadata[I]; ++i)
@@ -58,6 +54,8 @@ void AigParser::extract_literals(const std::vector<std::string>& aag_lines)
         split(aag_lines[i], ' ', parts);
         _out_literals.push_back(std::stoul(parts[0]));
     }
+
+    return *this;
 }
 
 std::string AigParser::aig_to_aag(const std::string& aig_path) // change someday to something less disgusting
@@ -70,7 +68,7 @@ std::string AigParser::aig_to_aag(const std::string& aig_path) // change someday
   return out_path;
 }
 
-void AigParser::read_aag(std::vector<std::string>& aag_container) const
+const AigParser& AigParser::read_aag(std::vector<std::string>& aag_container) const
 {
     std::string line;
     std::ifstream aag_file(_aag_path.c_str());
@@ -78,9 +76,11 @@ void AigParser::read_aag(std::vector<std::string>& aag_container) const
     {
       aag_container.push_back(line);
     }
+
+    return *this;
 }
 
-void AigParser::extract_metadata(const std::string& first_aag_line)
+AigParser& AigParser::extract_metadata(const std::string& first_aag_line)
 {
     std::vector<std::string> components;
     split(first_aag_line, ' ', components);
@@ -90,6 +90,8 @@ void AigParser::extract_metadata(const std::string& first_aag_line)
     _metadata[L] = std::stoul(components[3]);
     _metadata[O] = std::stoul(components[4]);
     _metadata[A] = std::stoul(components[5]);
+
+    return *this;
 }
 
 
@@ -99,7 +101,7 @@ const std::map<AigMetadata, size_t> &AigParser::get_aig_metadata()
     return _metadata;
 }
 
-void AigParser::extract_ap_mapping(const std::vector<std::string>& aag_lines)
+AigParser& AigParser::extract_ap_mapping(const std::vector<std::string>& aag_lines)
 {
     size_t first_ap_index = 0;
     bool found_ap = false;
@@ -120,40 +122,47 @@ void AigParser::extract_ap_mapping(const std::vector<std::string>& aag_lines)
     }
     assert(found_ap);
     _first_ap_index = first_ap_index;
+    return *this;
 }
 
 
-std::map<size_t, z3::expr> AigParser::get_literal_formulas(const std::vector<std::string> &aag_lines)
+AigParser& AigParser::calc_literal_formulas(const std::vector<std::string> &aag_lines)
 {
-    z3::context c;
 
-    _lit_formulas.insert(std::make_pair(0, c.bool_val(false)));
-    _lit_formulas.insert(std::make_pair(1, c.bool_val(true)));
-    for (auto lit : _in_literals) _lit_formulas.insert(std::make_pair(lit, c.bool_const(std::to_string(lit).data())));
-    for (auto lit : _prev_state_literals) _lit_formulas.insert(std::make_pair(lit, c.bool_const(std::to_string(lit).data())));
+
+    _lit_formulas.insert(std::make_pair(0, _formula_context.bool_val(false)));
+    _lit_formulas.insert(std::make_pair(1, _formula_context.bool_val(true)));
+    for (auto lit : _in_literals) _lit_formulas.insert(std::make_pair(lit, _formula_context.bool_const(std::to_string(lit).data())));
+    for (auto lit : _prev_state_literals) _lit_formulas.insert(std::make_pair(lit, _formula_context.bool_const(std::to_string(lit).data())));
 
     size_t first_and_line = _first_ap_index - _metadata[A];
     for (auto lit : _next_state_literals) dfs(aag_lines, _lit_formulas, first_and_line, lit);
     for (auto lit : _out_literals) dfs(aag_lines, _lit_formulas, first_and_line, lit);
 
+//    for (auto it : _lit_formulas) std::cout << it.first << " ~ " << it.second << std::endl;
+
+    return *this;
 }
 
-void AigParser::dfs(const std::vector<std::string> &lines, std::map<size_t, z3::expr>& formulas, size_t first_line, size_t target_lit) const
+const AigParser& AigParser::dfs(const std::vector<std::string> &lines, std::map<size_t, z3::expr>& formulas, size_t first_line, size_t target_lit) const
 {
-    if (formulas.find(target_lit) != formulas.end()) return;
+    if (formulas.find(target_lit) == formulas.end()) {
+        if (target_lit % 2 == 1) {
+            dfs(lines, formulas, first_line, target_lit - 1); // FOR NOW. DO OPT OF OR HERE!
+            formulas.insert(std::make_pair(target_lit, !formulas.at(target_lit - 1)));
+        } else {
+            size_t first_and_literal = (_metadata.at(AigMetadata::I) + _metadata.at(L) + 1) * 2;
+            const size_t and_line_index = first_line + (target_lit - first_and_literal) / 2;
+            const std::string &and_line = lines[and_line_index];
+            std::vector<std::string> parts;
+            split(and_line, ' ', parts);
+            size_t left_operand = std::stoul(parts[1]);
+            size_t right_operand = std::stoul(parts[2]);
 
-    if (target_lit % 2 == 1) {
-        dfs(lines, formulas, first_line, target_lit - 1); // FOR NOW. DO OPT OF OR HERE!
-        formulas.insert(std::make_pair(target_lit, !formulas.at(target_lit - 1)));
-    } else {
-        const std::string &and_line = lines[target_lit];
-        std::vector<std::string> parts;
-        split(and_line, ' ', parts);
-        size_t left_operand = std::stoul(parts[1]);
-        size_t right_operand = std::stoul(parts[2]);
-
-        dfs(lines, formulas, first_line, left_operand);
-        dfs(lines, formulas, first_line, right_operand);
-        formulas.insert(std::make_pair(target_lit, formulas.at(left_operand) && formulas.at(right_operand)));
+            dfs(lines, formulas, first_line, left_operand);
+            dfs(lines, formulas, first_line, right_operand);
+            formulas.insert(std::make_pair(target_lit, formulas.at(left_operand) && formulas.at(right_operand)));
+        }
     }
+    return *this;
 }
--- code/parsers/aig_parser.h
+++ code/parsers/aig_parser.h
@@ -26,10 +26,12 @@ public:
 
 private:
     std::string aig_to_aag(const std::string& aig_path0);
-    void read_aag(std::vector<std::string>& aag_container) const;
-    void extract_metadata(const std::string& first_aag_line);
-    void extract_literals(const std::vector<std::string>& aag_lines);
-    void dfs(const std::vector<std::string> &lines, std::map<size_t, z3::expr>& formulas, size_t first_line, size_t target_lit) const;
+    const AigParser& read_aag(std::vector<std::string>& aag_container) const;
+    AigParser& extract_metadata(const std::string& first_aag_line);
+    AigParser& extract_literals(const std::vector<std::string>& aag_lines);
+    const AigParser& dfs(const std::vector<std::string> &lines, std::map<size_t, z3::expr>& formulas, size_t first_line, size_t target_lit) const;
+    AigParser& extract_ap_mapping(const std::vector<std::string>& vector);
+    AigParser& calc_literal_formulas(const std::vector<std::string>& aag_lines);
 
     const std::string _aig_path;
     std::string _aag_path;
@@ -42,10 +44,8 @@ private:
     std::vector<size_t> _prev_state_literals;
     std::vector<size_t> _next_state_literals;
     std::map<size_t, z3::expr> _lit_formulas;
+    z3::context _formula_context;
 
-    void extract_ap_mapping(const std::vector<std::string>& vector);
-
-    std::map<size_t, z3::expr> get_literal_formulas(const std::vector<std::string>& aag_lines);
 };
 
 
